46ed97dc1d2fce9f7699528750ff85e0e205a9d2
ali <alimortazaviasl2026@gmail.com>
2026-08-18 08:55:23 +0330

    rm space

--- src/Components/features/UserSiderBarInformation/components/PersonalInformation.jsx
+++ src/Components/features/UserSiderBarInformation/components/PersonalInformation.jsx
@@ -34,7 +34,7 @@ function PersonalInformation() {
                  <span className='text-success'>availLable</span>
             </div>
             <div className='d-flex my-2  justify-content-between '>
-                 <span className='text-bg-warning p-1 rounded-1'>Address :</span>
+                 <span className='text-bg-warning p-1 rounded-1'>Address:</span>
                  <span>Tehran,Tehran</span>
             </div>
         </div>
